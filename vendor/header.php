54626b9ae8a1f2d23c0eb551c4a6d892dd5cce31
Mohamed Amine Lahiala <mohamedamine@lahiala.com>
2022-10-05 15:07:53 +0100

    ToolKit Menu
    added a toolkit menu with a dropdown

--- vendor/header.php
+++ vendor/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_base.css">
+    <link rel="stylesheet" href="./style_base.css">
 </head>
 <body>
     
@@ -19,7 +19,15 @@
             <a href="/news.php">News</a>
             <a href="/process.php">Process</a>
             <a href="/contact.php">Contact</a>
-            <a href="/toolkit/toolkit.php">ToolKit</a>
+            <div class="dropdown">
+                <a class="dropbtn" href="/toolkit/toolkit.php">ToolKit</a>
+                <div class="dropdown-content">
+                    <a href="/toolkit/attestation_stage.php">Internship Proof</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+</div>
         </div>
         </nav>
         <div class="right_menu">
