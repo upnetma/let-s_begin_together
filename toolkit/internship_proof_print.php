f2c249eeb335b343b0770b82f3067aa49ebd46fa
Mohamed Amine Lahiala <mohamedamine@lahiala.com>
2022-10-05 18:58:36 +0100

    Links colors
    * Links color changed to the branding one
    
    Co-Authored-By: upnetworkmaroc <102196215+upnetworkmaroc@users.noreply.github.com>

--- toolkit/internship_proof_print.php
+++ toolkit/internship_proof_print.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Internship Proof</title>
-    <link rel="stylesheet" href="style_base.css">
+    <link rel="stylesheet" href="../style_base.css">
 </head>
 <body>
 <?php //include '../vendor/header.html'?>
